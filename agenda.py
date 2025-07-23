44f3a21e26db3ad208b7753e5b18b81960292122
Mathy <65187882+Mistycy@users.noreply.github.com>
2025-07-23 02:06:49 -0300

    Update agenda.py

--- agenda.py
+++ agenda.py
@@ -16,33 +16,33 @@ def salvar_agendas(agendas):
 
 def adicionar_contato(agendas=None):
             agenda = carregar_agendas()
-            nome = input("nome do contato: ")
-            email = input("email do contato: ")
-            telefone = input("telefone do contato: ")
-            pessoa = {'nome': nome, "email": email, 'telefone': telefone}
+            nome = input("Nome do contato: ")
+            email = input("Email do contato: ")
+            telefone = input("Telefone do contato: ")
+            pessoa = {'Nome': nome, "Email": email, 'Telefone': telefone}
             agenda.append(pessoa)
             salvar_agendas(agenda)
-            print("contato adicionado com sucesso!")
+            print("Contato adicionado com sucesso!")
 
 
 def listar_contatos():
         agendas = carregar_agendas()
         print("\nLista de Contatos:")
         for i, contato in enumerate(agendas, 1):
-            nome = contato.get("nome", "")
-            email = contato.get("email", "")
-            telefone = contato.get("telefone", "")
+            nome = contato.get("Nome", "")
+            email = contato.get("Email", "")
+            telefone = contato.get("Telefone", "")
             print(f"{i}. Nome: {nome} | Email: {email} | Telefone: {telefone}")
 
 def apagar_contato(agendas=None):
 
     try:
         agendas = carregar_agendas()
-        indice = int(input("digite qual contato deseja remover: "))
+        indice = int(input("Digite a numeração do contato que deseja remover: "))
         if 1 <= indice <= len(agendas):
             contato_removido = agendas.pop(indice - 1)
             salvar_agendas(agendas)
-            print(f"contato: '{contato_removido}' removido com sucesso.")
+            print(f"contato: '{contato_removido}' Removido com sucesso.")
         else:
             print("Número inválido.")
     except ValueError:
@@ -57,7 +57,7 @@ def menu():
         print("3 - Listar contatos")
         print("4 - Sair")
         print("---------------------------")
-        escolha = int(input("qual opção você deseja? "))
+        escolha = int(input("Qual opção você deseja? "))
 
         if escolha == 1:
              adicionar_contato()
